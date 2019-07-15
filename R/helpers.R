39863f4ab8fa856ec0e2f1bc054a3b055d807afc
Siyuan Ma <sim653@mail.harvard.edu>
2019-07-15 14:57:27 -0400

    update documentation for the helper functions

--- R/helpers.R
+++ R/helpers.R
@@ -1,109 +1,137 @@
-#' @param n_ij feature count of sample i feature j
-#' @param n_i total read count of sample i
-#' 
-#' @return
+#' Initial per-feature logit normal mean estimation
 #'
-#' @examples
-get_muhat_i <- function(n_ij, n_i) {
-  n_ij <- addone_ifzero(n_ij)
-  n_ij <- minusone_ifmax(n_ij, n_i) ## FIXME
-  phat_i <- n_ij/n_i
+#' @param y_ij vector of per-feature read count across samples
+#' @param n_i vector of total read count across samples
+#'
+#' @return estimated log normal mean
+get_muhat_i <- function(y_ij, n_i) {
+  y_ij <- add_ifzero(y_ij, .5)
+  phat_i <- y_ij/n_i
   log(phat_i) - log(1 - phat_i)
 }
 
-#' @param n_ij feature count of sample i feature j
-#' @param n_i total read count of sample i
-#' 
-#' @return
+#' Initial per-feature logit normal variance estimation
+#' @param y_ij vector of per-feature read count across samples
+#' @param n_i vector of total read count across samples
 #'
-#' @examples
-get_sigma2hat_i <- function(n_ij, n_i) {
-  n_ij <- addone_ifzero(n_ij)
-  n_ij <- minusone_ifmax(n_ij, n_i) ## FIXME
-  phat_i <- n_ij/n_i
+#' @return estimated log normal variance
+get_sigma2hat_i <- function(y_ij, n_i) {
+  y_ij <- add_ifzero(y_ij, 0.5)
+  phat_i <- y_ij/n_i
   1 / (n_i * phat_i * (1 - phat_i))
 }
 
-#' @param n_ij feature count of sample i feature j
-#' 
-#' @return
+#' Add a fixed value to zero counts (mainly used for Laplacian approximation of binomial likelihood)
 #'
-#' @examples
-addone_ifzero <- function(n_ij) {
-  ind_zero_i <- n_ij == 0
-  n_ij[ind_zero_i] <- n_ij[ind_zero_i] + 1
-  n_ij
+#' @param y_ij vector of per-feature read count across samples
+#'
+#' @return modified counts
+add_ifzero <- function(y_ij, value) {
+  ind_zero_i <- y_ij == 0
+  y_ij[ind_zero_i] <- y_ij[ind_zero_i] + value
+  y_ij
 }
 
-#' @param n_ij feature count of sample i feature j
-#' @param n_i total read count of sample i
-#' 
-#' @return
-#'
-#' @examples
-minusone_ifmax <- function(n_ij, n_i) {
-  ind_max_i <- n_ij >= n_i
-  n_ij[ind_max_i] <- n_ij[ind_max_i] - 1
-  n_ij
-}
 
-#' @param muhat_i mu iteration
-#' @param sigma2hat_i sigma2 iteration
-#' @param n_ij feature count of sample i feature j
-#' @param n_i total read count of sample i
-#' @param pi0 
-#' @param mu 
-#' @param sigma2 
-#' 
-#' @return weights
+#' E step update for the per sample weights of prevalence parameter
 #'
-#' @examples
-get_w_i <- function(muhat_i, sigma2hat_i, 
-                    n_ij, n_i,
-                    pi0, mu, sigma2) {
-  ind_zero_i <- n_ij == 0
-  w_i <- rep(1, length = length(n_ij))
-  likelihood_normal_i <- dnorm(muhat_i[ind_zero_i], 
-                               mean = mu, 
+#' @param muhat_i vector of muhat estimation across samples
+#' @param sigma2hat_i vector of sigma2hat estimation across samples
+#' @param y_ij vector of per-feature read count across samples
+#' @param n_i vector of total read count across samples
+#' @param pi pi parameter (in current iteration)
+#' @param mu mu parameter (in current iteration)
+#' @param sigma2 sigma2 parameter (in current iteration)
+#'
+#' @return weights of observations, in the form as a) expectation (probability) of sequencing (as
+#' opposed to biological) zero for zero-count samples, and b) ones for non-zero-count samples
+get_w_i <- function(muhat_i, sigma2hat_i,
+                    y_ij, n_i,
+                    pi, mu, sigma2) {
+  ind_zero_i <- y_ij == 0
+  w_i <- rep(1, length = length(y_ij))
+  likelihood_normal_i <- dnorm(muhat_i[ind_zero_i],
+                               mean = mu,
                                sd = sqrt(sigma2hat_i[ind_zero_i] + sigma2))
-  w_i[ind_zero_i] <- 1 - pi0 / (pi0 + (1 - pi0) * likelihood_normal_i)
+  w_i[ind_zero_i] <- (pi * likelihood_normal_i) / (1 - pi + pi * likelihood_normal_i)
   w_i
 }
 
-update_pi0 <- function(w_i) mean(1 - w_i)
+#' M step update for pi
+#'
+#' @param w_i vector of observation weights
+#'
+#' @return updated pi estimation
+update_pi <- function(w_i) {
+  mean(w_i)
+}
 
+#' M step update for mu
+#'
+#' @param sigma2 sigma2 parameters
+#' @param muhat_i vector of muhat estimation across samples
+#' @param sigma2hat_i vector of sigma2hat estimation across samples
+#' @param w_i vector of observation weights
+#'
+#' @return updated mu estimation
 update_mu <- function(sigma2, muhat_i, sigma2hat_i, w_i) {
   weights_i <- w_i / (sigma2 + sigma2hat_i)
   sum(weights_i * muhat_i) / sum(weights_i)
 }
 
-update_sigma2 <- function(muhat_i, sigma2hat_i, w_i, sigma2_old) {
-  mu_sigma2is0_i <- update_mu(sigma2 = 0, 
+#' M step update for sigma2
+#'
+#' @param muhat_i vector of muhat estimation across samples
+#' @param sigma2hat_i vector of sigma2hat estimation across samples
+#' @param w_i vector of observation weights
+#' @param sigma2_old old sigma2 parameter (used as initial value for optimization)
+#' @param control list of control parameters for the inner layer optim function
+#'
+#' @return updated sigma2 estimation
+update_sigma2 <- function(muhat_i, sigma2hat_i, w_i, sigma2_old, control) {
+  mu_sigma2is0_i <- update_mu(sigma2 = 0,
                               muhat_i = muhat_i,
                               sigma2hat_i = sigma2hat_i,
                               w_i = w_i)
-  if(sum(sum(w_i / sigma2hat_i^2 * (mu_sigma2is0_i - muhat_i)^2)) <= 
+  if(sum(sum(w_i / sigma2hat_i^2 * (mu_sigma2is0_i - muhat_i)^2)) <=
      sum(w_i / sigma2hat_i))
     return(0)
   optim(par = sigma2_old,
-        fn = negLogLik_sigma2, 
+        fn = negLogLik_sigma2,
         gr = dNegLogLik_sigma2,
         muhat_i = muhat_i,
         sigma2hat_i = sigma2hat_i,
         w_i = w_i,
         method = "L-BFGS-B",
-        lower = 0, upper = Inf)$par
+        lower = 0, upper = Inf,
+        control = control)$par
 }
 
+#' Negative log likelihood with respect to sigma2
+#'
+#' @param sigma2 sigma2 parameter
+#' @param muhat_i vector of muhat estimation across samples
+#' @param sigma2hat_i vector of sigma2hat estimation across samples
+#' @param w_i vector of observation weights
+#'
+#' @return negative log likelihood, evaluated at sigma2
 negLogLik_sigma2 <- function(sigma2, muhat_i, sigma2hat_i, w_i) {
   mu_sigma2_i <- update_mu(sigma2 = sigma2,
                            muhat_i = muhat_i,
                            sigma2hat_i = sigma2hat_i,
                            w_i = w_i)
-  sum(w_i * log(sigma2 + sigma2hat_i)) + 
+  sum(w_i * log(sigma2 + sigma2hat_i)) +
     sum(w_i * (muhat_i - mu_sigma2_i)^2 / (sigma2 + sigma2hat_i))
 }
 
+#' Derivative of the negative log likelihood (for gradient decent)
+#'
+#' @param sigma2 sigma2 parameter
+#' @param muhat_i vector of muhat estimation across samples
+#' @param sigma2hat_i vector of sigma2hat estimation across samples
+#' @param w_i vector of observation weights
+#'
+#' @return  derivative of negative log likelihood, evaluated at sigma2
 dNegLogLik_sigma2 <- function(sigma2, muhat_i, sigma2hat_i, w_i) {
   mu_sigma2_i <- update_mu(sigma2 = sigma2,
                            muhat_i = muhat_i,
@@ -111,13 +139,27 @@ dNegLogLik_sigma2 <- function(sigma2, muhat_i, sigma2hat_i, w_i) {
                            w_i = w_i)
   sum(w_i / (sigma2 + sigma2hat_i)) -
     sum(w_i * (muhat_i - mu_sigma2_i)^2 / (sigma2 + sigma2hat_i)^2)
-  
+
 }
 
+#' Posterior estimation of per-sample mu (shrinked by the estimated parameters)
+#'
+#' @param muhat_i vector of muhat estimation across samples (before shrinkage)
+#' @param sigma2hat_i vector of sigma2hat estimation across samples (before shrinkage)
+#' @param mu estimated mu parameter
+#' @param sigma2 estimated sigma2 parameter
+#'
+#' @return posterior muhat_i, shrinked towards mu
 posterior_mu_i <- function(muhat_i, sigma2hat_i, mu, sigma2) {
-  (muhat_i * sigma2 + mu * sigma2hat_i) / (sigma2hat_i + sigma2) 
+  (muhat_i * sigma2 + mu * sigma2hat_i) / (sigma2hat_i + sigma2)
 }
 
+#' Posterior estimation of per-sample sigma2 (shrinked by the estimated parameters)
+#'
+#' @param sigma2hat_i vector of sigma2hat estimation across samples (before shrinkage)
+#' @param sigma2 estimated sigma2 parameter
+#'
+#' @return  posterior sigma2hat_i
 posterior_sigma2_i <- function(sigma2hat_i, sigma2) {
   sigma2hat_i * sigma2 / (sigma2hat_i + sigma2)
 }

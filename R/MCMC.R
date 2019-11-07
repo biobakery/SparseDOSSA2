dd4ee73a6f086585ccad774217192c63401de09e
Siyuan Ma <sim653@mail.harvard.edu>
2019-11-07 17:42:19 -0500

    mu need to be constrained to original values for identifiability; use a function instead of direct multiplication to calculate a

--- R/MCMC.R
+++ R/MCMC.R
@@ -1,4 +1,4 @@
-EM <- function(data, controls = list(ncores = 4,
+EM <- function(data, controls = list(ncores = 6,
                                      lambda = 0.2,
                                      R = 1000,
                                      burnin = 0.1,
@@ -18,6 +18,8 @@ EM <- function(data, controls = list(ncores = 4,
                  Omega = fit_copulasso$fits[[1]])
   
   i_iter <- 0
+  l_easums <- list()
+  ll_params <- list()
   while(TRUE) {
     i_iter <- i_iter + 1
     if(controls$verbose)
@@ -25,41 +27,41 @@ EM <- function(data, controls = list(ncores = 4,
     
     ## E step
     doParallel::registerDoParallel(cores = controls$ncores)
-    e_asums <- foreach::`%dopar%`(
-      foreach::foreach(i_sample = seq_len(nrow(data)),
-                       .combine='c'),
-      {
-        asum_samples <- mcmc_asum(x = data[i_sample, , drop = TRUE],
-                                  params = params,
-                                  R = controls$R)
-        mean(vapply(
-          asum_samples[-seq_len(round(controls$R * controls$burnin))],
-          function(i_asum) i_asum[["asum"]],
-          0.0))
-      })
-    
     # e_asums <- foreach::`%dopar%`(
     #   foreach::foreach(i_sample = seq_len(nrow(data)),
     #                    .combine='c'),
     #   {
-    #     asum_num <- integrate(vintegrand_num_asum, 
-    #                           lower = -20, upper = 20, ## FIXME
-    #                           x = data[i_sample, , drop = TRUE],
-    #                           params = params)
-    #     asum_denom <- integrate(vintegrand_denom_asum, 
-    #                             lower = -20, upper = 20, ## FIXME
-    #                             x = data[i_sample, , drop = TRUE],
-    #                             params = params)
-    #     asum_num$value / asum_denom$value
+    #     asum_samples <- mcmc_asum(x = data[i_sample, , drop = TRUE],
+    #                               params = params,
+    #                               R = controls$R)
+    #     mean(vapply(
+    #       asum_samples[-seq_len(round(controls$R * controls$burnin))],
+    #       function(i_asum) i_asum[["asum"]],
+    #       0.0))
     #   })
+    e_asums <- foreach::`%dopar%`(
+      foreach::foreach(i_sample = seq_len(nrow(data)),
+                       .combine='c'),
+      {
+        asum_num <- integrate(vintegrand_num_asum,
+                              lower = -20, upper = 20, ## FIXME
+                              x = data[i_sample, , drop = TRUE],
+                              params = params)
+        asum_denom <- integrate(vintegrand_denom_asum,
+                                lower = -20, upper = 20, ## FIXME
+                                x = data[i_sample, , drop = TRUE],
+                                params = params)
+        asum_num$value / asum_denom$value
+      })
     doParallel::stopImplicitCluster()
+    l_easums[[i_iter]] <- e_asums
     
     ## M step
     a_data <- data * e_asums
-    a_data[is.na(e_asums), ] <- 
-      data[is.na(e_asums), ] * 
-      mean(e_asums, na.rm = TRUE) ## FIXME
-    fit_marginals <- get_marginals(a_data)
+    # a_data[is.na(e_asums), ] <- 
+    #   data[is.na(e_asums), ] * 
+    #   mean(e_asums, na.rm = TRUE) ## FIXME
+    fit_sigmas <- get_sigmas(a_data, params$mu)
     fit_copulasso <- copulasso(data = a_data, 
                                lambda_list = controls$lambda,
                                K_CV = NULL, ## FIXME
@@ -67,10 +69,10 @@ EM <- function(data, controls = list(ncores = 4,
     )
     params_new <- list(pi0 = fit_marginals[, 1],
                        mu = fit_marginals[,2],
-                       sigma = fit_marginals[, 3],
+                       sigma = fit_sigmas,
                        Omega = fit_copulasso$fits[[1]])
     
-    diff <- vapply(seq_along(params_new),
+    diff <- vapply(seq(from = 3, to = length(params_new)),
                    function(i_param) {
                      max(abs(params_new[[i_param]] - params[[i_param]]))
                    },
@@ -82,6 +84,7 @@ EM <- function(data, controls = list(ncores = 4,
     if(i_iter > controls$maxit)
       stop("Maximum iteration reached!")
     
+    ll_params[[i_iter]] <- params_new
     params <- params_new
   }
 }
@@ -116,9 +119,10 @@ one_step_asum <- function(asum = NULL, logLik = NULL,
   else 
     asum_star <- exp(log(asum) + rnorm(n = 1, sd = 1)) ## FIXME
   
-  u_star <- a_to_u(asum_star * x, 
+  u_star <- a_to_u(a(x = x, asum = asum_star), 
                    pi0 = params$pi0, mu = params$mu, sigma = params$sigma)
-  g_star <- u_to_g(u_star)
+  g_star <- u_to_g(u_star, a = a(x = x, asum = asum_star), 
+                   mu = params$mu, sigma = params$sigma)
   logLik_star <- logLik_copula(g = g_star, asum = asum_star, x = x,
                                mu = params$mu, sigma = params$sigma, 
                                Omega = params$Omega)
@@ -174,7 +178,7 @@ integrand_num_asum <- function(log_asum, x, params) {
   asum <- exp(log_asum)
   u <- a_to_u(a(x, asum), 
               pi0 = params$pi0, mu = params$mu, sigma = params$sigma)
-  g <- u_to_g(u)
+  g <- u_to_g(u = u, a = a(x, asum), mu = params$mu, sigma = params$sigma)
   logLik <- logLik_copula(g = g, asum = asum, x = x,
                           mu = params$mu, sigma = params$sigma, 
                           Omega = params$Omega)
@@ -190,7 +194,7 @@ integrand_denom_asum <- function(log_asum, x, params) {
   asum <- exp(log_asum)
   u <- a_to_u(a(x, asum),
               pi0 = params$pi0, mu = params$mu, sigma = params$sigma)
-  g <- u_to_g(u)
+  g <- u_to_g(u = u, a = a(x, asum), mu = params$mu, sigma = params$sigma)
   logLik <- logLik_copula(g = g, asum = asum, x = x,
                           mu = params$mu, sigma = params$sigma, 
                           Omega = params$Omega)
@@ -206,4 +210,13 @@ a <- function(x, asum) {
   a[x > 0] <- x[x > 0] * asum
   
   return(a)
+}
+
+get_sigmas <- function(data, mu) {
+  vapply(seq_len(ncol(data)),
+         function(i_sample) {
+           sqrt(mean((log((data[, i_sample])[data[, i_sample] > 0]) - 
+                        mu[i_sample])^2))
+         },
+         0.0)
 }

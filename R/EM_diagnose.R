9efdf06778a6e91e52d4f0fc3c41d89aa5048aa3
siyuanma <sim653@mail.harvard.edu>
2020-02-25 20:19:05 +0000

    update EM fit functions for CV

--- R/EM_diagnose.R
+++ R/EM_diagnose.R
@@ -10,6 +10,165 @@
 #'
 #' @examples
 EM_diagnose <- function(data, 
+                        lambdas,
+                        control = list()) {
+  
+  control <- do.call(control_EM, control)
+  if(!is.null(control$debug_dir))
+    dir.create(control$debug_dir)
+  
+  # Initialize using relative abundances
+  fit_marginals <- get_marginals(data)
+  
+  l_results <- list()
+  for(i_lambda in seq_along(lambdas)) {
+    if(control$verbose) message("Fitting for lambda ", i_lambda)
+    lambda <- lambdas[i_lambda]
+    
+    l_results[[i_lambda]] <- future::future({
+      
+      # Initialize using relative abundances
+      fit_copulasso <- copulasso(data = data, 
+                                 lambda_list = lambda,
+                                 K_CV = NULL) ## FIXME
+      params <- list(pi0 = fit_marginals[, 1],
+                     mu = fit_marginals[,2],
+                     sigma = fit_marginals[, 3],
+                     Sigma = solve(fit_copulasso$fits[[1]]),
+                     Omega = fit_copulasso$fits[[1]])
+      
+      i_iter <- 0
+      converge <- FALSE
+      ll_easums <- list()
+      ll_params <- list()
+      while(TRUE) {
+        time <- Sys.time()
+        if(i_iter + 1 > control$maxit) break
+        i_iter <- i_iter + 1
+        if(control$verbose)
+          message("EM iteration ", i_iter)
+        
+        ## E step
+        e_asums <- future.apply::future_vapply(
+          seq_len(nrow(data)),
+          function(i_sample) {
+            i_time <- Sys.time()
+            if(i_iter == 1) offset_a <- 1
+            else offset_a <- ll_easums[[i_iter - 1]][i_sample, 1]
+            num <- ea(x = data[i_sample, , drop = TRUE],
+                      pi0 = params$pi0, mu = params$mu, 
+                      sigma = params$sigma, Omega = params$Omega,
+                      offset_a = offset_a,
+                      control = c(control$control_numint, 
+                                  list(only_value = FALSE,
+                                       proper = FALSE)))
+            eloga_num <- eloga(x = data[i_sample, , drop = TRUE],
+                               pi0 = params$pi0, mu = params$mu, 
+                               sigma = params$sigma, Omega = params$Omega,
+                               offset_a = offset_a,
+                               control = c(control$control_numint, 
+                                           list(only_value = FALSE,
+                                                proper = FALSE)))
+            eloga2_num <- eloga2(x = data[i_sample, , drop = TRUE],
+                                 pi0 = params$pi0, mu = params$mu, 
+                                 sigma = params$sigma, Omega = params$Omega,
+                                 offset_a = offset_a,
+                                 control = c(control$control_numint, 
+                                             list(only_value = FALSE,
+                                                  proper = FALSE)))
+            denom <- dx(x = data[i_sample, , drop = TRUE],
+                        pi0 = params$pi0, mu = params$mu, 
+                        sigma = params$sigma, Omega = params$Omega,
+                        offset_a = offset_a,
+                        control = c(control$control_numint, 
+                                    list(only_value = FALSE,
+                                         proper = FALSE)))
+            l <- log_dx(x = data[i_sample, , drop = TRUE],
+                        pi0 = params$pi0, mu = params$mu,
+                        sigma = params$sigma, Omega = params$Omega,
+                        offset_a = offset_a,
+                        control = control$control_numint)
+            return(c("mean" = num$integral / denom$integral,
+                     "error" = abs(num$error / denom$integral) + 
+                       abs(num$integral / (denom$integral)^2 * 
+                             denom$error),
+                     "l" = l,
+                     "eloga" = eloga_num$integral / denom$integral,
+                     "eloga2" = eloga2_num$integral / denom$integral,
+                     "time" = Sys.time() - i_time))
+          },
+          rep(0.0, 6)
+        ) %>% t()
+        ll_easums[[i_iter]] <- e_asums
+        
+        ## M step
+        a_data <- (data * e_asums[, 1])[!is.na(e_asums[, 1]), ] ## FIXME
+        fit_sigmas <- get_sigmas(x = data, 
+                                 eloga = e_asums[, "eloga"], 
+                                 eloga2 = e_asums[, "eloga2"], 
+                                 mu = fit_marginals[, 2])
+        fit_copulasso <- copulasso(data = a_data, 
+                                   lambda_list = lambda,
+                                   K_CV = NULL) ## FIXME
+        params_new <- list(pi0 = fit_marginals[, 1],
+                           mu = fit_marginals[, 2],
+                           sigma = fit_sigmas,
+                           Sigma = solve(fit_copulasso$fits[[1]]),
+                           Omega = fit_copulasso$fits[[1]])
+        
+        diff_abs <- vapply(c("sigma", "Sigma"), 
+                           function(i_param)
+                             get_diff(params_new[[i_param]], params[[i_param]], 
+                                      denom_c = control$abs_tol, method = "abs"),
+                           0.0)
+        diff_rel <- vapply(c("sigma", "Sigma"), 
+                           function(i_param)
+                             get_diff(params_new[[i_param]], params[[i_param]], 
+                                      denom_c = control$abs_tol, method = "rel"),
+                           0.0)
+        
+        ll_params[[i_iter]] <- c(params_new,
+                                 list(diff = c(diff_abs, diff_rel),
+                                      l = mean(ll_easums[[i_iter]][, 3]),
+                                      time = Sys.time() - time))
+        params <- params_new
+        
+        if(!is.null(control$debug_dir)) {
+          l_debug <- list(ll_easums = ll_easums, ll_params = ll_params)
+          save(l_debug,
+               file = paste0(control$debug_dir,"debug_", i, ".RData"))
+        }
+        
+        if(max(diff_abs) < control$abs_tol & max(diff_rel) < control$rel_tol) {
+          converge <- TRUE
+          break
+        }
+      }
+      
+      list(lambda = lambda,
+           fit = ll_params[[i_iter]],
+           convergence = list(converge = converge,
+                              n_iter = i_iter))
+    })
+  }
+  
+  l_results <- future::values(l_results)
+  return(l_results)
+}
+
+
+#' Title
+#'
+#' @param data 
+#' @param params_init 
+#' @param control 
+#'
+#' @return
+#' @export
+#' @importFrom magrittr %>%
+#'
+#' @examples
+EM_diagnose_CV <- function(data, 
                         params_init = NULL,
                         control = list()) {
   control <- do.call(control_EM, control)
@@ -150,7 +309,7 @@ EM_diagnose <- function(data,
       save(res_tmp,
            file = control$debug)
     }
-           
+    
     if(max(diff_abs) < control$abs_tol & max(diff_rel) < control$rel_tol) {
       converge <- TRUE
       break
@@ -162,13 +321,9 @@ EM_diagnose <- function(data,
                               n_iter = i_iter)))
 }
 
-control_EM <- function(lambda = 0.2,
-                       maxit = 1000,
-                       method = "numint",
+control_EM <- function(maxit = 1000,
                        rel_tol = 1e-3,
                        abs_tol = 1e-3,
-                       control_mcmc = list(R = 10000,
-                                           burnin = 0.1),
                        control_numint = list(subdivisions = 10000,
                                              limit_max = 50,
                                              limit_min = 1e-10,
@@ -176,14 +331,12 @@ control_EM <- function(lambda = 0.2,
                                              rel_tol = 1e-6,
                                              abs_tol = 0),
                        verbose = FALSE,
-                       debug = NULL) {
-  list(lambda = lambda,
-       maxit = maxit,
-       method = method,
+                       debug_dir = NULL) {
+  list(maxit = maxit,
        rel_tol = rel_tol,
        abs_tol = abs_tol,
        control_mcmc = control_mcmc,
        control_numint = control_numint,
        verbose = verbose,
-       debug = debug)
+       debug_dir = debug_dir)
 }

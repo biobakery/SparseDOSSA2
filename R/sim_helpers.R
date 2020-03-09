5af36985d68655e5dc7dc9b85f2a48816aaab684
siyuanma <sim653@mail.harvard.edu>
2020-03-09 23:06:43 +0000

    simulation evaluation helper for getting induced x parameters from a parameters with Monte Carlo

--- R/sim_helpers.R
+++ R/sim_helpers.R
@@ -2,8 +2,8 @@ filter_params <- function(params, max_pi0 = 0.95) {
   ind <- params$pi0 < max_pi0
   Omega <- solve(params$Sigma[ind, ind])
   return(list(pi0 = params$pi0[ind],
-              mu = params$pi0[ind],
-              sigma = params$pi0[ind],
+              mu = params$mu[ind],
+              sigma = params$sigma[ind],
               Omega = solve(params$Sigma[ind, ind]),
               Sigma = params$Sigma[ind, ind]))
 }
@@ -26,4 +26,18 @@ simulate_a <- function(n, params, k_feature = 2, max_iter = 10, verbose = FALSE)
       return(samples_a)
     }
   }
+}
+
+params_a_to_x <- function(params_a, n = 100000) {
+  samples_a <- rcopulasso(n = n,
+                          pi0 = params_a$pi0,
+                          mu = params_a$mu,
+                          sigma = params_a$sigma,
+                          Omega = params_a$Omega)
+  samples_x <- t(apply(samples_a, 1, function(x) x / sum(x)))
+  mat_marginals <- get_marginals(samples_x)
+  return(list(pi0 = mat_marginals[, "pi0"],
+              mu = mat_marginals[, "mu"],
+              sigma = mat_marginals[, "sigma"],
+              Corr = cor(samples_x, method = "spear")))
 }

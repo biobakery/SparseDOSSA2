eff7968ef5938b638b9abbf9cee1d165a21538db
siyuanma <sim653@mail.harvard.edu>
2020-05-11 21:49:18 +0000

    * Interface tweaks on per-feature parameter estimations. * Use log instead of logit transformation. * Add additional per-feature g space paramters to help with inverting from P_star to P during M step estimation of Omega

--- R/estimate_new.R
+++ R/estimate_new.R
@@ -1,24 +1,38 @@
 estimate_featureParam_new <- function(x) {
-  ind_nonzero <- x > 0 & x < 1
-  if(all(ind_nonzero)) ## FIXME
-    pi0 <- length(ind_nonzero) / (length(ind_nonzero) + 0.5)
-  else
-    pi0 <- mean(!ind_nonzero)
-  mu <- mean(log(x[ind_nonzero]) - log(1 - x[ind_nonzero])) ## FIXME
+  ind_nonzero <- x > 0
+  if(sum(ind_nonzero) <= 1) ## FIXME
+    stop("Does not support features with only one non-zero value or less!")
+  if(all(!ind_nonzero)) ## FIXME
+    stop("Does not support features with all non-zero values!")
+  
+  pi0 <- mean(!ind_nonzero)
+  mu <- mean(log(x[ind_nonzero]))
   if(sum(ind_nonzero) > 1)
-    sigma <- sd(log(x[ind_nonzero]) - log(1 - x[ind_nonzero]))
+    sigma <- sd(log(x[ind_nonzero]))
   else
     sigma <- 1 ##FIXME
   
+  # Additional parameters to help with computation
+  glim <- qnorm(pi0)
+  g0 <- truncnorm::etruncnorm(b = glim)
+  sigmaMod <- sqrt(pi0 * g0^2 + 
+                     (1 - pi0) * 
+                     (truncnorm::vtruncnorm(a = glim) + 
+                        truncnorm::etruncnorm(a = glim)^2))
+  
   return(c("pi0" = pi0,
            "mu" = mu,
-           "sigma" = sigma))
+           "sigma" = sigma,
+           "glim" = glim,
+           "g0" = g0,
+           sigmaMod = sigmaMod))
 }
 
 get_marginals <- function(X) {
-  t(vapply(seq_len(ncol(X)),
-           function(i_feature)
-             estimate_featureParam_new(
-               X[, i_feature, drop = TRUE]),
-           c(0.0, 0.0, 0.0)))
+  as.data.frame(
+    t(vapply(seq_len(ncol(X)),
+             function(i_feature)
+               estimate_featureParam_new(
+                 X[, i_feature, drop = TRUE]),
+             rep(0.0, 6))))
 }

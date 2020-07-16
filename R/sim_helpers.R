6ace94102d096e37cf938f70499a3930e6e76f48
siyuanma <sim653@mail.harvard.edu>
2020-07-16 21:48:19 +0000

    Remove legacy code

--- R/sim_helpers.R
+++ R/sim_helpers.R
@@ -7,26 +7,6 @@ filter_params <- function(params, max_pi0 = 0.95) {
               Sigma = params$Sigma[ind, ind]))
 }
 
-simulate_a <- function(n, params, k_feature = 2, maxit = 10, verbose = FALSE) {
-  i_iter <- 0
-  while(TRUE) {
-    if(i_iter + 1 > maxit)
-      stop("Can't satisfy conditions!")
-    i_iter <- i_iter + 1
-    
-    samples_a <- rcopulasso(n = n,
-                            pi0 = params$pi0,
-                            mu = params$mu,
-                            sigma = params$sigma,
-                            Omega = params$Omega)
-    
-    if(all(apply(samples_a > 0, 2, sum) > 1)) {
-      if(verbose) print(i_iter)
-      return(samples_a)
-    }
-  }
-}
-
 params_a_to_x <- function(params_a, n = 100000) {
   samples_a <- rcopulasso(n = round(n * 1.5), # FIXME
                           pi0 = params_a$pi0,

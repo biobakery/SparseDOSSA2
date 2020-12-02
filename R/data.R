5d1ef109c8138f8f47862467a15f2aa2c0519f54
siyuanma <sim653@mail.harvard.edu>
2020-12-02 01:37:49 +0000

    include datasets

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' A subset of the HMP1-II stool samples
+#'
+#' A dataset containing species-level microbial counts of a subset of
+#' the HMP1-II stool samples. This includes the top 5 most abundant
+#' species and top 5 most deeply sequenced samples.
+#'
+#' @format A matrix with 5 rows (species) and 5 columns (samples)
+#' @source \url{https://www.hmpdacc.org/hmp/}
+"Stool_subset"
